c417898a78840047cd38362886d6981ad38e6ff1
Adam Mukhriz Bin Yusoff <adammukhrizbackup@gmail.com>
2024-05-29 13:56:32 +0800

    create window,player,platform,background

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import sys
 
 # Initialize Pygame
 pygame.init()
+
+# Constants
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+WORLD_WIDTH, WORLD_HEIGHT = 1600, 600  # Increased width for horizontal scrolling
+PLAYER_WIDTH, PLAYER_HEIGHT = 50, 50
+PLAYER_COLOR = (0, 0, 255)
+PLATFORM_COLOR = (0, 255, 0)
+BACKGROUND_COLOR = (0, 0, 0)
