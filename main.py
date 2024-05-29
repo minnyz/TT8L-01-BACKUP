9c5feb2e449d2f64bf71abf108b6daca97d4f4c7
Adam Mukhriz Bin Yusoff <adammukhrizbackup@gmail.com>
2024-05-29 13:59:49 +0800

    background image and display

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ PLAYER_WIDTH, PLAYER_HEIGHT = 50, 50
 PLAYER_COLOR = (0, 0, 255)
 PLATFORM_COLOR = (0, 255, 0)
 BACKGROUND_COLOR = (0, 0, 0)
+
+# Load background image
+background_image = pygame.image.load("background1.jpg")  # Replace "your_background_image.jpg" with your image file
+background_image = pygame.transform.scale(background_image, (WORLD_WIDTH, WORLD_HEIGHT))
+
+# Set up the display
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("World(test)")
