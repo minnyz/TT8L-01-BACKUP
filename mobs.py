ab583e68427a0c4072a6ab3a64cd7b4400cc9ed0
muhaimin <12211068100@student.mmu.edu.my>
2024-05-29 16:22:46 +0800

    putting each image on spritesheet to blit 1 by 1

--- mobs.py
+++ mobs.py
@@ -1,66 +1,43 @@
 import pygame
-import random
-import math
-
+import spritesheet
 
 pygame.init()
 
-
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Drone NPC Movement")
+SCREEN_HEIGHT = 800
 
-# Colors
-WHITE = (255, 255, 255)
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Spritesheets')
+
+sprite_sheet_image = pygame.image.load('assets/mobsLow/1/Idle.png').convert_alpha()
+sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
+
+BG =(50, 50, 50)
 BLACK = (0, 0, 0)
 
-clock = pygame.time.Clock()
+frame_0 = sprite_sheet.get_image( 0, 48, 48, 3, BLACK)
+frame_1 = sprite_sheet.get_image( 1, 48, 48, 3, BLACK)
+frame_2 = sprite_sheet.get_image( 2, 48, 48, 3, BLACK)
+frame_3 = sprite_sheet.get_image( 3, 48, 48, 3, BLACK)
 
-class DroneNPC(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("assets/mobsLow/1/Idle.png").convert_alpha()
-        self.image = self.image
-        self.rect = self.image.get_rect()
-        self.rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-        
-        self.speed = 2
-        self.angle = 0
-        self.angular_speed = 0.05
-
-    def update(self):
-        self.image = pygame.transform.rotate(self.image, pygame.time.get_ticks() / 10 * self.angular_speed)
-        self.rect = self.image.get_rect(center=self.rect.center)
-        
-        self.image = pygame.transform.rotate(self.image, math.degrees(-self.angle))
-        
-        
-        if self.rect.left > SCREEN_WIDTH:
-            self.rect.right = 0
-        elif self.rect.right < 0:
-            self.rect.left = SCREEN_WIDTH
-        if self.rect.top > SCREEN_HEIGHT:
-            self.rect.bottom = 0
-        elif self.rect.bottom < 0:
-            self.rect.top = SCREEN_HEIGHT
-
-all_sprites = pygame.sprite.Group()
-drone = DroneNPC()
-all_sprites.add(drone)
-
-running = True
-while running:
+run = True
+while run:
+    
+    #background
+    screen.fill(BG)
+    
+    #display npc
+    screen.blit(frame_0, (0,0))
+    screen.blit(frame_1, (130,0))
+    screen.blit(frame_2, (260,0))
+    screen.blit(frame_3, (390,0))
+    
+    #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-
-    # Update all sprites
-    all_sprites.update()
-    screen.fill(BLACK)
-    all_sprites.draw(screen)
-    pygame.display.flip()
-
+            run=False
+            
+    pygame.display.update()
 pygame.quit()
 
 
