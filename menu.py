f5e7e503213b512bf6d0c82273e28553b46b3e09
muhaimin <12211068100@student.mmu.edu.my>
2024-06-05 15:21:49 +0800

    Fixing a certain line to link with main

--- menu.py
+++ menu.py
@@ -1,6 +1,6 @@
 import pygame, sys
 from button import Button
-
+from pygame import mixer
 
 pygame.init()
 
@@ -9,45 +9,25 @@ pygame.mixer.init()
 click_sound =  pygame.mixer.Sound("assets/enterface_click_2.mp3")
 
 # Create the screen and set the title
-SCREEN = pygame.display.set_mode((1280, 720))
+SCREEN = pygame.display.set_mode((1280, 720), pygame.FULLSCREEN)
 pygame.display.set_caption("Menu")
 
-# for background
+
+# for Background
 BG = pygame.image.load("assets/Background.png")
 
+# for Background Sound
+#mixer.music.load("assets/Wild_Hunt.wav")
+#mixer.music.play(-1)
+
+# Font 1
 def get_font_1(size): 
     return pygame.font.Font("assets/cyb3.ttf", size)
 
+# Font 2
 def get_font_2(size):
     return pygame.font.Font("assets/cyb_options.otf", size)
-
-def play():
-    while True:
-        PLAY_MOUSE_POS = pygame.mouse.get_pos()
-
-        SCREEN.blit(BG, (0, 0))
-
-        PLAY_TEXT = get_font_1(45).render("PLAY screen.", True, "White")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
-        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
-
-        PLAY_BACK = Button(image=None, pos=(640, 460), 
-                            text_input="BACK", font=get_font_1(75), base_color="White", hovering_color="Red")
-
-        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
-        PLAY_BACK.update(SCREEN)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
-                    click_sound.play()
-                    main_menu()
-
-        pygame.display.update()
-    
+   
 def options():
     while True:
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
@@ -67,7 +47,7 @@ def options():
         KEY_BUTTON = Button(image=None, pos=(318, 400),
                             text_input="Key Bindings", font=get_font_1(75), base_color="White", hovering_color="Yellow")   
 
-        OPTIONS_BACK = Button(image=None, pos=(180, 500), 
+        OPTIONS_BACK = Button(image=None, pos=(180, 600), 
                             text_input="Back", font=get_font_1(75), base_color="White", hovering_color="Red")
         
         for button in [AUDIO_BUTTON, OPTIONS_BACK, VIDEO_BUTTON, KEY_BUTTON]:    
@@ -101,7 +81,11 @@ def audio():
         
         SCREEN.blit(BG,(0, 0))
         
-        OPTIONS_BACK = Button(image=None, pos=(180, 500), 
+        PLAY_TEXT = get_font_1(110).render("Audio", True, "#b68f40")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 60))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+               
+        OPTIONS_BACK = Button(image=None, pos=(180, 600), 
                             text_input="Back", font=get_font_1(75), base_color="White", hovering_color="Red")
         
         for button in [OPTIONS_BACK]:    
@@ -125,7 +109,11 @@ def video():
         
         SCREEN.blit(BG,(0, 0))
         
-        OPTIONS_BACK = Button(image=None, pos=(180, 500), 
+        PLAY_TEXT = get_font_1(110).render("Video", True, "#b68f40")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 60))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+        
+        OPTIONS_BACK = Button(image=None, pos=(180, 600), 
                             text_input="Back", font=get_font_1(75), base_color="White", hovering_color="Red")
         
         for button in [OPTIONS_BACK]:    
@@ -150,7 +138,11 @@ def key():
         
         SCREEN.blit(BG,(0, 0))
         
-        OPTIONS_BACK = Button(image=None, pos=(180, 500), 
+        PLAY_TEXT = get_font_1(100).render("Key Bindings", True, "#b68f40")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 60))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+        
+        OPTIONS_BACK = Button(image=None, pos=(180, 600), 
                             text_input="Back", font=get_font_1(75), base_color="White", hovering_color="Red")
         
         for button in [OPTIONS_BACK]:    
@@ -197,7 +189,8 @@ def main_menu():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
                     click_sound.play()
-                    play()
+                    import main
+                    main.main()
                 if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
                     click_sound.play()
                     options()
