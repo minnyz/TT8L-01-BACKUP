ce5c5c13bab97f79a72cd472044a1d2dfa3ed360
WAN HAKIMI AMSYAR BIN WAN KHAIRUL AZMIR <hakimiamsyar88@gmail.com>
2024-06-11 18:23:10 +0800

    Adding background to intro text.
    C:\Neon Veil\TT8L-01-BACKUP>git log
    commit 93156d50d80a256697776d1ea8f0dfe64c5b7e86 (HEAD -> Wan_Hakimi_Amsyar)
    Author: Wan Hakimi Amsyar <hakimiamsyar88@gmail.com>
    Date:   Tue Jun 11 17:49:01 2024 +0800

--- plot.py
+++ plot.py
@@ -2,13 +2,14 @@ import pygame
 
 pygame.init()
 
-SCREEN_WIDTH = 1200
+SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT),  pygame.FULLSCREEN)
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN)
 pygame.display.set_caption("Plot")
 
 Background = pygame.image.load("Assets/Background.png")
+Background = pygame.transform.scale(Background, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 intro_font = pygame.font.Font("Assets/cyberpunk_font.ttf", 45)
 detail_font = pygame.font.Font("Assets/cyberpunk_font.ttf", 30)
@@ -17,25 +18,40 @@ def intro_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
 
-def detail_text(text, font, text_col, x, y):
-    img = font.render(text, True, text_col)
-    screen.blit(img, (x, y))
+def wrap_text(text, font, max_width):
+    lines = []
+    words = text.split(' ')
+    while words:
+        line = ''
+        while words and font.size(line + words[0])[0] <= max_width:
+            line += (words.pop(0) + ' ')
+        lines.append(line)
+    return lines
+
+def detail_text(text, font, text_col, x, y, max_width):
+    lines = wrap_text(text, font, max_width)
+    for i, line in enumerate(lines):
+        img = font.render(line, True, text_col)
+        screen.blit(img, (x, y + i * font.get_linesize()))
 
 run = True
 while run:
-
-    screen.fill((255,255,255))
-
-    intro_text("Introduction", intro_font, (0, 0, 0), 50, 50)
-    detail_text('In the year 2179, the neon-lit mega-city of Echelon is a bustling \
-                metropolis of towering skyscrapers and hidden dangers. Detective Riley Crane, a former corporate \
-                security expert turned rogue investigator, is called to investigate the mysterious murder of a high-ranking \
-                executive from Orion Industries. The crime scene is a baffling mix of ancient symbols and \
-                disabled high-tech security.', detail_font, (0, 0, 0), 50, 150)
-
     for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        run = False
+        if event.type == pygame.QUIT:
+            run = False
+
+    # Draw the background image
+    screen.blit(Background, (0, 0))
+
+    # Render the introduction text
+    intro_text("Introduction", intro_font, (0, 0, 255), 50, 50)
+    
+    # Render the detailed plot text with wrapping
+    detail_text('In the year 2179, the neon-lit mega-city of Echelon is a bustling '
+                'metropolis of towering skyscrapers and hidden dangers. Detective Riley Crane, a former corporate '
+                'security expert turned rogue investigator, is called to investigate the mysterious murder of a high-ranking '
+                'executive from Orion Industries. The crime scene is a baffling mix of ancient symbols and '
+                'disabled high-tech security.', detail_font, (255, 0, 255), 50, 150, SCREEN_WIDTH - 100)
 
     pygame.display.flip()
 
